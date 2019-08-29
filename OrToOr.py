aef7bbbf9a7ee2ed213899f39f92869371262d0d
romoreira <rodrigo.moreira206@gmail.com>
2019-08-29 16:05:42 -0300

    Introducing Or-Or Interface. Preliminary Steps - Geode

---
+++ OrToOr.py
@@ -0,0 +1,32 @@
+'''
+Author: Rodrigo Moreira
+Date: 29-08-2019 - 09:41
+'''
+#HThis class will interact with Geode: https://geode.apache.org/docs/guide/19/getting_started/15_minute_quickstart_gfsh.html
+
+class NSDManagement():
+    def __init__(self):
+        print("NSDManagement Object Created")
+
+class NSLifeCycleManagement():
+    def __init__(self):
+        print("NSLifeCycleManagement Object Created")
+
+class NSLifecycleOperationGranting():
+    def __init__(self):
+        print("NSLifecycleOperationGranting Object Created")
+
+class NSInstanceUsage():
+    def __init__(self):
+        print("NSInstanceUsage Object Created")
+
+class NSPerformanceManagment():
+    def __init__(self):
+        print("NSPerformanceManagement Object Created")
+
+class NSFaultManagement():
+    def __init__(self):
+        print("NSFaultManagement Object Created")
+
+if __name__ == '__main__':
+    obj = NSLifeCycleManagement()
