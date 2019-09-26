cf75a9c20db57f6f771b03a7b0b74d837fa5b7f8
romoreira <rodrigo.moreira206@gmail.com>
2019-09-26 20:11:47 -0300

    Building MANO towards OSM RestAPI.

--- MANO.py
+++ MANO.py
@@ -3,12 +3,84 @@ Autor: Rodrigo Moreira
 Date: 06/09/2019
 """
 
+#https://opendev.org/x/microstack
+
+import yaml, requests, json, logging, glob, os
+
+#logging.debug('This is a debug message')
+#logging.info('This is an info message')
+#logging.warning('This is a warning message')
+#logging.error('This is an error message')
+#logging.critical('This is a critical message')
+#logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 class MANO:
-    VNFD = None
 
-    def __init__(self, VNFD):
-        self.VNFD = VNFD
+    NST = ""
+    NSD = ""
+    OSM_TOKEN_ID = ""
 
-    def vnfd_yaml_interpreter(self):
+
+
+    def __init__(self, NST):
+        self.NST = NST
+
+    def read_nsd(self, nsd_dir_name):
+        os.chdir(nsd_dir_name)
+        nsd_file_name = ""
+        for nsd in glob.glob("*.yaml"):
+            nsd_file_name = str(nsd)
+
+        nsd_full_path = nsd_dir_name + str("\\") + nsd_file_name
+
+        with open(nsd_full_path, 'r') as stream:
+            NSD = yaml.safe_load(stream)
+            self.NSD = NSD
+
+            with open('result.yaml', 'w') as yaml_file:
+                yaml.dump(self.NSD, yaml_file, default_flow_style=False)
+
+        print(self.NSD)
+
+    def get_tokenid(self, osm_response):
+        data = osm_response.text.splitlines()
+        id = data[4].split("id: ",1)[1]
+        self.OSM_TOKEN_ID = id
+        if id == None:
+            logging.error('Getting Token ID from OSM')
+            return None
+        return id
+
+    def osm_connector(self):
+
         print("Here we will use OSM Rest API to Lauch VNF Instances")
+        try:
+            osm_host = "10.8.0.1"
+            osm_port = "9999"
+            url = "https://"+osm_host+":"+osm_port+"/osm/admin/v1/tokens"
+            data = {"username": "admin", "password": "admin"}
+            headers = {"Content-type": "application/json", "Accept": "text/plain"}
+            r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
+
+            logging.debug('Authentication OSM: '+str(osm_host) + ' status code: '+str(r.status_code))
+
+            if int(r.status_code) != 200 and int(r.status_code) != 201:
+                logging.error("OSM Authentication Fail")
+                return
+        except:
+            logging.error("OSM "+osm_host+" Unreacheable")
+            return
+
+        """
+        Getting Token ID
+        """
+        print(str(self.get_tokenid(r)))
+
+if __name__ == "__main__":
+    mano_worker = MANO()
+    #mano_worker.read_nsd_temporary()
+    #mano_worker.osm_connector()
+    #mano_worker.vnfd_untar()
+    #print(str(mano_worker.nsd_untar()))
+    mano_worker.read_nsd(r"C:\Users\morei\PycharmProjects\InterOIB\ns\cirros_2vnf_ns")
--- ServiceBuilder.py
+++ ServiceBuilder.py
@@ -5,6 +5,7 @@ Date: 06/09/2019
 
 # Each Service Builder in each Domain is able to receive NST to proced with Service Deployment
 
+import requests, json, logging, glob, os
 import yaml
 import NANO
 import MANO
@@ -14,8 +15,51 @@ class ServiceBuilder:
     NSD = None
     VNFD = None
 
+    def nsd_untar(self):
+        dir = os.path.dirname("./ns/cirros_2vnf_ns.tar.gz")  # get directory where file is stored
+        dir_extract = os.path.dirname("./ns/")
+        filename = os.path.basename("./ns/cirros_2vnf_ns.tar.gz")  # get filename
+        file_tar, file_tar_ext = os.path.splitext("./ns/cirros_2vnf_ns.tar.gz")  # split into file.tar and .gz
+        file_untar, file_untar_ext = os.path.splitext(file_tar)  # split into file and .tar
+        os.chdir(dir)
+        if file_tar_ext == ".gz" and file_untar_ext == ".tar":  # check if file had format .tar.gz
+            import tarfile
+            tar = tarfile.open(filename)
+            tar.extractall(path=dir_extract)  # untar file into same directory
+            tar.close()
+            os.chdir(
+                file_untar)  # This fails if file.tar.gz has different name compared to the untarred folder e.g.. file1 instead of file
+
+            logging.debug("Uncompressed NSD " + str(filename) + " on " + str(file_untar))
+
+            # Dir name of extracted files
+            return file_untar
+
+    """
+    Receives a tar.gz VNFD and returns a directory name of extracted files
+    """
+
+    def vnfd_untar(self):
+        dir = os.path.dirname("./ns/cirros_vnf.tar.gz")  # get directory where file is stored
+        dir_extract = os.path.dirname("./ns/")
+        filename = os.path.basename("./ns/cirros_vnf.tar.gz")  # get filename
+        file_tar, file_tar_ext = os.path.splitext("./ns/cirros_vnf.tar.gz")  # split into file.tar and .gz
+        file_untar, file_untar_ext = os.path.splitext(file_tar)  # split into file and .tar
+        os.chdir(dir)
+        if file_tar_ext == ".gz" and file_untar_ext == ".tar":  # check if file had format .tar.gz
+            import tarfile
+            tar = tarfile.open(filename)
+            tar.extractall(path=dir_extract)  # untar file into same directory
+            tar.close()
+            os.chdir(file_untar)  # This fails if file.tar.gz has different name compared to the untarred folder e.g.. file1 instead of file
+
+            logging.debug("Uncompressed VNFD " + str(filename) + " on " + str(file_untar))
+
+            # Dir name of extracted files
+            return file_untar
+
     def read_nsd(self):
-        with open("./vnf-descriptor/cirros_2vnf_ns/cirros_2vnf_nsd.yaml", 'r') as stream:
+        with open("./ns/cirros_2vnf_ns/cirros_2vnf_nsd.yaml", 'r') as stream:
             NSD = yaml.safe_load(stream)
 
         self.NSD = NSD['nstd:nstd-details']
@@ -26,16 +70,20 @@ class ServiceBuilder:
         nmano.nst_yaml_interpreter()
 
     def virtual_network_function_description(self):
+        print(self.VNFD)
+        return
         mano = MANO.MANO(self.VNFD)
         mano.vnfd_yaml_interpreter()
 
 
 if __name__ == "__main__":
     sb = ServiceBuilder()
-    sb.read_nsd()
+    #sb.read_nsd()
 
     """
     Splitting YAML service descriptor to OSM and NANO to provide Network Slice Builder
     """
-    sb.network_slice_template()
-    sb.virtual_network_function_description()
+    #sb.network_slice_template()
+    #sb.virtual_network_function_description()
+    #sb.vnfd_untar()
+    sb.nsd_untar()
