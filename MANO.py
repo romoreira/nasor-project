50f6dd1e516ba6b8fdb55e4da5166d2d63c37011
romoreira <rodrigo.moreira206@gmail.com>
2019-09-27 10:07:59 -0300

    Building MANO towards OSM RestAPI.

--- MANO.py
+++ MANO.py
@@ -17,16 +17,48 @@ import yaml, requests, json, logging, glob, os
 
 class MANO:
 
-    NST = ""
     NSD = ""
+    VNFD = ""
     OSM_TOKEN_ID = ""
 
 
 
-    def __init__(self, NST):
-        self.NST = NST
+    def __init__(self, NSD, VNFD):
+        self.NSD = ""
+        self.VNFD = ""
 
+    def post_nsd(self, OSM_IP, REQUEST_ID):
+        osm_host = "10.8.0.1"
+        osm_port = "9999"
+        url = "https://" + osm_host + ":" + osm_port + "/osm/nsd/v1/ns_descriptors_content"
+        headers = {"Content-type": "application/gzip", "Accept": "text/plain",
+                   'Authorization': 'Bearer {}'.format(self.OSM_TOKEN_ID)}
+        try:
+            with open('./ns/cirros_2vnf_ns.tar.gz', 'rb') as data:
+                r = requests.post(url, data=data, headers=headers, verify=False)
+                logging.debug('Posting NSD to OSM: ' + str(osm_host) + ' status code: ' + str(r.status_code))
+        except requests.exceptions.Timeout as ct:
+            logging.error(str(ct) + '\nFailed to Upload NSD to OSM - Connection Timeout:' + str(osm_host))
+        except requests.exceptions.RequestException as re:
+            logging.error(str(re) + '\nFailed to upload NSD to OSM' + str(osm_host))
 
+        return
+
+    def post_vnfd(self, OSM_IP, REQUEST_ID):
+        osm_host = "10.8.0.1"
+        osm_port = "9999"
+        url = "https://" + osm_host + ":" + osm_port + "/osm/vnfpkgm/v1/vnf_packages_content"
+        headers = {"Content-type": "application/gzip", "Accept": "text/plain", 'Authorization': 'Bearer {}'.format(self.OSM_TOKEN_ID)}
+        try:
+            with open('./ns/cirros_vnf.tar.gz', 'rb') as data:
+                r = requests.post(url, data=data, headers=headers, verify=False)
+                logging.debug('Posting VNFD to OSM: '+str(osm_host) + ' status code: '+str(r.status_code))
+        except requests.exceptions.Timeout as ct:
+            logging.error(str(ct) + '\nFailed to Upload VNFD to OSM - Connection Timeout:' + str(osm_host))
+        except requests.exceptions.RequestException as re:
+            logging.error(str(re)+'\nFailed to upload VNFD to OSM'+str(osm_host))
+
+        return
 
     def get_tokenid(self, osm_response):
         data = osm_response.text.splitlines()
@@ -38,8 +70,7 @@ class MANO:
         return id
 
     def osm_connector(self):
-
-        print("Here we will use OSM Rest API to Lauch VNF Instances")
+        logging.debug("OSM_CONNECTOR: Here we will use OSM Rest API to Lauch VNF Instances")
         try:
             osm_host = "10.8.0.1"
             osm_port = "9999"
@@ -53,19 +84,18 @@ class MANO:
             if int(r.status_code) != 200 and int(r.status_code) != 201:
                 logging.error("OSM Authentication Fail")
                 return
+            else:
+                return str(self.get_tokenid(r))
         except:
             logging.error("OSM "+osm_host+" Unreacheable")
             return
 
-        """
-        Getting Token ID
-        """
-        print(str(self.get_tokenid(r)))
-
 if __name__ == "__main__":
-    mano_worker = MANO()
+    mano_worker = MANO("","")
     #mano_worker.read_nsd_temporary()
     #mano_worker.osm_connector()
     #mano_worker.vnfd_untar()
     #print(str(mano_worker.nsd_untar()))
-    mano_worker.read_nsd(r"C:\Users\morei\PycharmProjects\InterOIB\ns\cirros_2vnf_ns")
+    mano_worker.osm_connector()
+    mano_worker.post_vnfd("","")
+    mano_worker.post_nsd("", "")
