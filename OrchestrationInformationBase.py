dd76f3b30c904d2be71bec787b76e3c1031ffe2a
romoreira <rodrigo.moreira206@gmail.com>
2019-10-15 10:07:04 -0300

    Building Inter-Domain Data-Plane by using Segment-Routing. Apache Geode API

--- OrchestrationInformationBase.py
+++ OrchestrationInformationBase.py
@@ -2,7 +2,8 @@
 Author: Rodrigo Moreira
 Date: 14/10/2019
 """
-import requests, logging, json
+import requests, logging, json, time, os
+import pandas as pd
 
 
 class OrchestrationInformationBase:
@@ -102,13 +103,33 @@ class OrchestrationInformationBase:
 
         return
 
-    def initialize_oib_geode(self, region, key):
+    def geode_syncronization(self):
 
-        data = """[string: %s]"""
+        csv_file = pd.DataFrame(pd.read_csv("./data/inter-orchestrator_information_base.csv", sep=",", header=0, index_col=False))
+        csv_file.to_json("file.json", orient="records", date_format="epoch", double_precision=10,
+                         force_ascii=True, date_unit="ms", default_handler=None)
 
-        data = str(data%("teste"))
 
-        data = json.dumps(data)
+        with open('file.json') as f:
+            data = json.load(f)
+
+        try:
+            os.remove('file.json')
+        except:
+            logging.error("Apache Geode - CSV to JSON Parsing Error - Deleting temporary file error")
+            return None
+
+        if data == "" or data == None:
+            logging.info("Apache Geode - CSV to JSON parsing error - JSON file is empty")
+            return
+
+        return data
+
+    def insert_datakey_into_region(self, region, key):
+        data = self.geode_syncronization()
+        if data == "" or data == None:
+            logging.error("Apache Geode - Error to Insert data to Repository - Data Empty")
+            return
 
         geode_host = "10.8.0.1"
         geode_port = "8080"
@@ -117,21 +138,29 @@ class OrchestrationInformationBase:
         headers = {"Content-Type": "application/json", "accept": "application/json"}
         try:
 
-            r = requests.put(url, data=data, headers=headers, verify=False)
+            r = requests.put(url, data=json.dumps(data), headers=headers, verify=False)
 
-            print(str(r.text))
+
+            if r.status_code != 200 and r.status_code != 201:
+                logging.error("Apache Geode - Inserting data on Repository Error - JSON")
+            else:
+                t = time.localtime()
+                current_time = time.strftime("%H:%M:%S", t)
+                print(str("Data Inserted on Apache Geode"))
+                logging.info("Data Inserted on Apache Geode. IP: "+str(geode_host) + " Data: "+current_time)
 
         except requests.exceptions.Timeout as ct:
-            logging.error(str(ct) + "Definir")
+            logging.error(str(ct) + "Apache Geode - Inserting data on Repository - Timeout")
         except requests.exceptions.RequestException as re:
-            logging.error(str(re) + "Definir")
+            logging.error(str(re) + "Apache Geode - Inserting data on Repository - Request Error")
 
         return
 
 if __name__ == "__main__":
     oib = OrchestrationInformationBase()
-    #oib.initialize_oib_geode("regionA",1)
+    #oib.geode_syncronization()
+    oib.insert_datakey_into_region("regionA",1)
     #oib.get_data_from_region("regionA")
     #oib.get_region_keys("regionA")
     #oib.get_region_servers("")
-    oib.delete_data_from_region("regionA", 0)
+    #oib.delete_data_from_region("regionA", 1)
