06f9fe35e18ae782dfa5301e97a645cd9c02d669
romoreira <rodrigo.moreira206@gmail.com>
2019-11-22 13:37:10 -0300

    Missing end2end-slice stop.

--- benchmark/lw-dns_bench.sh
+++ benchmark/lw-dns_bench.sh
@@ -2,3 +2,5 @@
 while true; do dig www.google.com | grep time; sleep 1; done
 
 #sudo apt-get install namebench - maybe i can use that
+#http://www.damagehead.com/blog/2015/04/28/deploying-a-dns-server-using-docker/
+#https://blog.webernetz.net/benchmarking-dns-namebench-dnseval/
