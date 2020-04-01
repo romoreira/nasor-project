588984b1f7ec6b4cdf4e973b451b26333b11c067
romoreira <rodrigo.moreira206@gmail.com>
2020-04-01 17:25:55 -0300

    SlicePolicy with Pycos: vnstat already included in the routers and PolicyAPI is Running. Missing: put gRPC in routers, install SIDs. Not working yet!

--- LessUsedInterfacePolicy.py
+++ LessUsedInterfacePolicy.py
@@ -7,60 +7,95 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-r = requests.get('http://127.0.0.1:5000/v1.0/getTopology')
-#print("\n Response: "+str(r.content))
-resposta = str(r.text.decode("utf-8"))
-#resposta = resposta.replace("\"", "'")
-#jsongraph = json.dumps(json.loads(r.content.encode("utf-8")), sort_keys=True)
-#jsongraph = str.replace(str(jsongraph),"u'","'")
+class LUIP():
 
 
-jsongraph = json.loads(resposta)
-jsongraph = json.dumps(jsongraph)
-jsongraph = json.loads(jsongraph,'utf-8')
-#print(str(jsongraph["nodes"][0]["id"]).decode("utf-8"))
+    def topology_operations(self):
+
+        r = requests.get('http://127.0.0.1:5000/v1.0/getTopology')
+        #print("\n Response: "+str(r.content))
+        resposta = str(r.text.decode("utf-8"))
+        #resposta = resposta.replace("\"", "'")
+        #jsongraph = json.dumps(json.loads(r.content.encode("utf-8")), sort_keys=True)
+        #jsongraph = str.replace(str(jsongraph),"u'","'")
 
 
-print(jsongraph["nodes"][0])
-
-g = nx.DiGraph()
-
-for key in jsongraph:
-    if key == "links":
-        links = jsongraph[key]
-        for link in links:
-            #print(link)
-            g.add_edge(link["source"], link["target"], attr_dict=link["attr_dict"])
-
-#print("\nLinks:")
-#print(g.edges(data=True))
+        jsongraph = json.loads(resposta)
+        jsongraph = json.dumps(jsongraph)
+        jsongraph = json.loads(jsongraph,'utf-8')
+        #print(str(jsongraph["nodes"][0]["id"]).decode("utf-8"))
 
 
-# Add node attributes
-for key in jsongraph:
-    if key == "nodes":
-        routers = jsongraph[key]
-        for router in routers:
-            print(router)
-            g.node[router['id']].update(router)
 
+        print(jsongraph["nodes"][0])
 
-print("\nRoteadores")
-print(g.nodes(data=True))
+        g = nx.DiGraph()
 
-print("\nAll Paths:")
-for path in all_simple_paths(g, 'r1', 'r3'):
-    print (path)
-    print("Test if the path bandwidth consumption is lowest")
-
-for node1, node2, data in g.edges(data=True):
-    print(data["attr_dict"])
-
-
-#Path chosen according Djikstra will be the palce where the Slice SID-based will be setted.
-print("\nDjikstra")
-print(nx.dijkstra_path(g,'r1','r3',weight = lambda u, v, d: d['attr_dict']['network_metric'] ))
+        for key in jsongraph:
+            if key == "links":
+                links = jsongraph[key]
+                for link in links:
+                    #print(link)
+                    g.add_edge(link["source"], link["target"], attr_dict=link["attr_dict"])
 
 
 
 
+        # Add node attributes
+        for key in jsongraph:
+            if key == "nodes":
+                routers = jsongraph[key]
+                for router in routers:
+                    print(router)
+                    g.node[router['id']].update(router)
+
+
+
+
+        print("\nRoteadores")
+        print(g.nodes(data=True))
+        print("\nLinks:")
+        print(g.edges(data=True))
+
+
+
+        print("\nAll Paths:")
+        for path in all_simple_paths(g, 'r1', 'r3'):
+
+            print("Path a ser consultado: "+str(path))
+            #for each in path:
+            #    print("Item do Caminho: "+str(each))
+            #    for node in g.nodes(data=True):
+            #        print("Detalhes do No do Caminho: "+str(node))
+            #        if node[1:][0]["type"] == "core":
+            #            print(node[1:][0]["management_ip"])
+                #print("Cada Item do Caminho: "+str(each))
+        for node1, node2, data in g.edges(data=True):
+            print("Cada Item do Edges: "+str(data))
+
+        print("\nPos supostamente fazer o update")
+        for node1, node2, data in g.edges(data=True):
+            color = nx.get_edge_attributes(g, data[1:])
+            print("Cor: "+str(color))
+            nx.set_edge_attributes(g, data["attr_dict"]["color"],'g')
+            print("Cada Item do Edges: "+str(data))
+
+
+    def choose_path():
+
+        #Path chosen according Djikstra will be the palce where the Slice SID-based will be setted.
+        print("\nDjikstra")
+        print(nx.dijkstra_path(g,'r1','r3',weight = lambda u, v, d: d['attr_dict']['network_metric'] ))
+
+
+
+
+    def teste():
+        print("Ola mundo")
+
+if __name__ == '__main__':
+    print('Running by IDE - NANO')
+    topo = LUIP()
+    topo.topology_operations()
+else:
+    print('Imported in somewhere place - NANO')
