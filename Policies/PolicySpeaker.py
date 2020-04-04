885e46d46920e073e4f75eb9c5cc1615c535e77a
romoreira <rodrigo.moreira206@gmail.com>
2020-04-04 09:14:41 -0300

    Working well. Ready to run the experiments phase 2

--- Policies/PolicySpeaker.py
+++ Policies/PolicySpeaker.py
@@ -101,6 +101,7 @@ def sr_policy_installR4():
     sid_installer = grpc_sid_client.gRPC_SID("192.168.0.250", 123456, message)
     print("Policy SID Creation Response: " + str(sid_installer.main()))
 
+
 def route_installR4():
     data = """[{"paths": [{"via": "2607:f0d0:2001::1", "device": "eth2", "destination": "a::/64", "encapmode": "encap", "segments": ["1::d6", "2607::"]}]}]"""
     global message
@@ -112,16 +113,144 @@ def route_installR4():
     grpc_route_agent = grpc_client.gRPC_Route("192.168.0.250",12345,message)
     print(grpc_route_agent.main())
 
+
+
+
+
+
+
+
+
+
+
+    #-------------------------------------#Experimento 2 - Canal Alternativo#-------------------------------------------------
+
+
+
+
+
+
+
+
+
+def exp2_policy_installR1():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    data = """{%ssid_ip%s: %s""" + str("1::d6") + """%s, %ssid_behaviour%s: %s""" + str("end.dx6") + """%s, %sip_addr%s: %s""" + str("a::2") + """%s, %starget_if%s: %s""" + str("veth1_1") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.247", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+
+def exp2_policy_installR2_left_right():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    #sudo srconf localsid add 2::AD6:F1 end.ad6 ip 2:f1::f1 veth1_2 veth1_2
+    data = """{%ssid_ip%s: %s""" + str("2::") + """%s, %ssid_behaviour%s: %s""" + str("end") + """%s, %sip_addr%s: %s""" + str("") + """%s, %starget_if%s: %s""" + str("") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.248", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+def exp2_policy_installR3_left_right():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    #sudo srconf localsid add 2::AD6:F1 end.ad6 ip 2:f1::f1 veth1_2 veth1_2
+    data = """{%ssid_ip%s: %s""" + str("3::") + """%s, %ssid_behaviour%s: %s""" + str("end") + """%s, %sip_addr%s: %s""" + str("") + """%s, %starget_if%s: %s""" + str("") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.249", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+
+def exp2_policy_installR3_right_left():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    #sudo srconf localsid add 2::AD6:F1 end.ad6 ip 2:f1::f1 veth1_2 veth1_2
+    data = """{%ssid_ip%s: %s""" + str("5::") + """%s, %ssid_behaviour%s: %s""" + str("end") + """%s, %sip_addr%s: %s""" + str("") + """%s, %starget_if%s: %s""" + str("") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.249", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+
+def exp2_policy_installR2_right_left():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    #sudo srconf localsid add 2::AD6:F1 end.ad6 ip 2:f1::f1 veth1_2 veth1_2
+    data = """{%ssid_ip%s: %s""" + str("6::") + """%s, %ssid_behaviour%s: %s""" + str("end") + """%s, %sip_addr%s: %s""" + str("") + """%s, %starget_if%s: %s""" + str("") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.248", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+def exp2_policy_installR4():
+    #data = """[{"paths": [{"via": "1:2::2", "device": "eth1", "destination": "b::/64", "encapmode": "encap", "segments": ["3::D6","2::AD6:F2","2::AD6:F1"]}]}]"""
+    data = """{%ssid_ip%s: %s""" + str("4::d6") + """%s, %ssid_behaviour%s: %s""" + str("end.dx6") + """%s, %sip_addr%s: %s""" + str("b::2") + """%s, %starget_if%s: %s""" + str("veth1_3") + """%s}"""
+    data = str(data % ("\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\"", "\""))
+    global message
+    json_m = json.loads(data)
+
+    message = json_m
+
+    #print("Message SENT: "+str(message))
+
+    sid_installer = grpc_sid_client.gRPC_SID("192.168.0.250", 123456, message)
+    print("Policy SID Creation Response: " + str(sid_installer.main()))
+
+
 if __name__ == "__main__":
+
     logging.debug('Running by IDE - PolicySpeaker')
+
+    # Experimento 1-------------------------------------------------------------------
+
     #route_installR1()
     #sr_policy_installR1()
     #policy_installR3_left_right()
     #policy_installR3_right_left()
     #routes_installR3_left_right()
     #routes_installR3_right_left()
-    route_installR4()
+    #route_installR4()
     #sr_policy_installR4()
+
+    #Experimento 2-------------------------------------------------------------------
+
+    #exp2_policy_installR2_left_right()
+    #exp2_policy_installR3_left_right()
+    #exp2_policy_installR4()
+
+    #exp2_policy_installR3_right_left()
+    #exp2_policy_installR2_right_left()
+    #exp2_policy_installR1()
+
+
 
 else:
     logging.debug('Running throug import - PolicySpeaker')
